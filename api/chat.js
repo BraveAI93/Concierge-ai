518b8326ccaee62adcb8edda6d97cd21f1b8f188
BraveAI93 <aversabruno93@gmail.com>
2026-06-01 20:24:38 +0100

    Change export to module.exports in chat.js

--- api/chat.js
+++ api/chat.js
@@ -57,7 +57,7 @@ function getRateLimit(ip) {
   return true;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Owner-Key");
